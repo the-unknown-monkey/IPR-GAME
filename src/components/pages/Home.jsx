6b8a2177224b8aa616115db151a389309ddff4a8
Bijil <connectwithbijilkp@gmail.com>
2023-10-27 21:42:47 +0530

    first commit

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <>
+    <h1>HOME1</h1>
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem sunt
+        itaque, perferendis necessitatibus corrupti ea magni voluptatibus est
+        ipsa. Nulla praesentium dolore eos alias incidunt, odio et eaque, sunt
+        vitae corporis animi, itaque architecto. Sunt minima nostrum fugiat
+        quibusdam voluptate dolore illo cupiditate quam odio sequi. Consequatur
+        tempora ullam, ipsam molestias dolor dolores tempore. Molestias, error
+        porro tenetur delectus numquam animi aut quod consequuntur? Dolor sequi
+        quisquam ex, eaque explicabo, labore maxime veniam voluptas enim
+        recusandae architecto est voluptates optio libero officiis consectetur
+        possimus voluptate ipsam alias velit veritatis reprehenderit
+        repudiandae. Ullam praesentium inventore minima eius dolorem omnis fugit
+        facilis.
+      </div>
+      <br />
+    
+    
+    
+    
+    </>
+  );
+};
